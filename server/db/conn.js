c9e97065d5c08c862f5891f29cf59246b0d3a618
subhenduswe <63897474+subhenduswe@users.noreply.github.com>
2022-06-27 09:18:32 +0530

    Update conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 
-const DB = "mongodb+srv://subhendukunduswe:subhendu@cluster0.wz4p3ep.mongodb.net/mernstack?retryWrites=true&w=majority";
+const DB = "mongodb+srv://subhendukundu:subhendu@cluster0.wz4p3ep.mongodb.net/mernstack?retryWrites=true&w=majority";
 
 mongoose.connect(DB,{
     useNewUrlParser:true,
